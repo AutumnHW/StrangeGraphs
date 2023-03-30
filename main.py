a4a18f380182f09a5f18cb28aef64dba0f4847d5
Autumn Williams <63067844+AutumnHW@users.noreply.github.com>
2023-03-30 11:56:56 -0400

    setting up render templates

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #modules importing
-from flask import Flask
+from flask import Flask, render_template
 import igraph as ig
 import matplotlib.pyplot as plot
 g = ig.Graph()
 app = Flask(__name__)
 print(g)
 @app.route('/')
-def index():
+def home():
     #return 'Woo strange graphs attempt 2 electric boogaloo'
-    return "StrangeGraphs"
-    print("Testing 123")
+    return render_template('./main.html')
+app.run(debug=True)
