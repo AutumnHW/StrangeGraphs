51225b9dbb7bc80fb6d08949ac6bb4143f9575eb
Autumn Williams <63067844+AutumnHW@users.noreply.github.com>
2023-03-30 11:49:07 -0400

    Import changes, making changes to init code, and some testing.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+#modules importing
 from flask import Flask
-
+import igraph as ig
+import matplotlib.pyplot as plot
+g = ig.Graph()
 app = Flask(__name__)
-
+print(g)
 @app.route('/')
 def index():
-    return 'Woo strange graphs attempt 2 electric boogaloo'
-
-app.run()
+    #return 'Woo strange graphs attempt 2 electric boogaloo'
+    return "StrangeGraphs"
+    print("Testing 123")
